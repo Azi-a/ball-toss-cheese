eb1d0e3326dd936ad0124ada06dea2d1fe2bd2a5
Azi-a <auxentiusdawn@gmail.com>
2019-05-06 17:15:47 -0500

    Update TossViewer.java

--- TossViewer.java
+++ TossViewer.java
@@ -1,4 +1,51 @@
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Panel;
 
-public class TossViewer {
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
+
+
+/**
+ * 
+ * @author Zayd Moosajee, Jyotishka Sen, Ram Krishna
+ *
+ */
+public class TossViewer 
+{
+
+	public static void main(String[] args)
+	{
+		
+		JFrame frame = new JFrame("Ball_Toss");
+		
+		frame.setLayout(new FlowLayout());
+		
+		Button pickle = new Button("Pickle Man");
+		Button turtle = new Button("Turtonne");
+		Button moon = new Button("C-Moon");
+		Button king = new Button("Waffle King");
+		
+		frame.setBounds(400, 400, 800, 600);
+		
+		JOptionPane pane = new JOptionPane("Choose a character");
+		
+		frame.add(pane);
+		
+		frame.add(pickle,0);
+		frame.add(turtle,1);
+		frame.add(moon,2);
+		frame.add(king,3);
+		
+		
+	
+		
+		frame.setVisible(true);
+	}
+	
+	
+	
+	
+	
 }
