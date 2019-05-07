10c289206c177c4c576b0c781c04a7726c0d4a6d
Azi-a <auxentiusdawn@gmail.com>
2019-05-07 11:43:59 -0500

    Update Picture class
    basically just imports and a skeleton method

--- Picture.java
+++ Picture.java
@@ -1,4 +1,27 @@
+import javax.imageio.ImageIO;
 
-public class Picture {
+import java.awt.Image;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+public class Picture 
+{
+	
+	
+	
+	
 
+	/**
+	 * Generate an Image with the given codename. All sprites are located in the Main Sprites folder.
+	 * @param codename
+	 * @return
+	 */
+	public Image loadPicture(String codename)
+	{
+		return null;
+	}
+	
+	
+	
 }
