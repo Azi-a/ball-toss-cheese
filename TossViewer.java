9c2a75c1ece3ac47826014b3bac77fe4cca425df
Azi-a <auxentiusdawn@gmail.com>
2019-05-13 20:24:27 -0500

    Update TOsdfiedszcnfhdzgvk rdg

--- TossViewer.java
+++ TossViewer.java
@@ -1,19 +1,23 @@
-import java.awt.JButton;
+import java.awt.Button;
 import java.awt.FlowLayout;
-import java.awt.JPanel;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-
-
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 /**
  * 
  * @author Zayd Moosajee, Jyotishka Sen, Ram Krishna
- *
+ * 
  */
 public class TossViewer 
 {
+	
 	private static final double BASE_T = 0.15;
 	private static final int MS_S = 1000;
 
@@ -29,34 +33,35 @@ public class TossViewer
 		JButton moon = new JButton("C-Moon");
 		JButton king = new JButton("Waffle King");
 		
-		Thrower throw;
+		Thrower throwr;
 		class ThrowListener implements ActionListener
 		{
 			public void actionPerformed(ActionEvent event)
 			{
-				throw = new Thrower(event.getActionCommand());
+				throwr = new Thrower(event.getActionCommand());
 			}	
 		}	
 		JButton starter = new JButton("Start Throw");
-		JButton throw = new JButton("Throw Ball");
+		JButton throwbutton = new JButton("Throw Ball");
 		long startT = 0;
 		long endT = 0;
 		class StartListener implements ActionListener
 		{
-			public void actionPerformend(ActionEvent event)
-			{
-				if (event.getActionCommand().equals("Start Throw"))
+			@Override
+				public void actionPerformed(ActionEvent event)
 				{
-					startT = System.currentTimeMillis();
+					if (event.getActionCommand().equals("Start Throw"))
+					{
+						startT = System.currentTimeMillis();
+					}
+					else
+					{
+						endT = System.currentTimeMillis();
+					}
 				}
-				else
-				{
-					endT = System.currentTimeMillis();
-				}
-			}
 		}	
 		double changeT = (Math.abs(endT - startT)) / MS_S + BASE_T;
-		throw.setT(changeT);
+		throwr.setT(changeT);
 		
 		frame.setBounds(400, 400, 800, 600);
 		
@@ -70,16 +75,78 @@ public class TossViewer
 		frame.add(king,3);
 		
 		frame.add(starter, 4);
-		frame.add(throw, 5);
+	//	frame.add(throwr, 5);
 		
 		
 	
 		
 		frame.setVisible(true);
 	}
+		
+		
+		
+		
+		
+		
+		
+	
+/*		JFrame frame = new JFrame("Ball_Toss");
+		frame.setLayout(new FlowLayout());
+		frame.setBounds(400, 400, 800, 600);
+	
+		JButton pickle = new JButton("Pickle Man");
+		JButton turtle = new JButton("Turtonne");
+		JButton moon = new JButton("C-Moon");
+		JButton king = new JButton("Waffle King");
+		
+		JLabel text = new JLabel("Choose a character:");
+		JPanel textpanel = new JPanel();
+		textpanel.add(text);
+		
+		
+		
+		JOptionPane pane = new JOptionPane();
+		
+		JPanel panel = new JPanel(new FlowLayout());
+		
+		
+		
+		turtle.setEnabled(false);
+		moon.setEnabled(false);
+		king.setEnabled(false);
+		
+		panel.add(pickle);
+		panel.add(turtle);
+		panel.add(moon);
+		panel.add(king);
+	
+		frame.add(textpanel,0);
+		frame.add(panel,1);
+		
+		frame.setVisible(true);
+		
+		
+		
+		Picture pickleman = new Picture("pm");
+		
+		JFrame frame2 = new JFrame("Ball_Toss");
+		
+		frame.setLayout(new FlowLayout());
+		
+
+		*/
+		
 	
 	
 	
+	
+	
+	
+	public void throwBall(Thrower player,Ball baseball)
+	{
+		
+		
+	}
 	
 	
 }
