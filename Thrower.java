2b4842651de40bd18c54df14830066c6273e484e
Azi-a <auxentiusdawn@gmail.com>
2019-05-13 20:24:57 -0500

    Update Thrower.java

--- Thrower.java
+++ Thrower.java
@@ -1,4 +1,24 @@
 
-public class Thrower {
+public class Thrower 
+{
+	private String ability;
+	private double time;
+	
+	public Thrower(String power)
+	{
+		ability = power;
+	}
+	
+	
+	public String getAbility()
+	{
+		return this.ability;
+	}
 
+
+	public void setT(double changeT) 
+	{
+		time = changeT;
+	}
+	
 }
